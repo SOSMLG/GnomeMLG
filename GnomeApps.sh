25942795c79ab3b8062ca945ba46e3bf02072660
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-08-24 07:53:15 -0400

    Update GnomeApps.sh

--- GnomeApps.sh
+++ GnomeApps.sh
@@ -11,7 +11,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 sudo apt install git -yy
 bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
 # installing Other Apps 
-sudo apt install vlc micro libreoffice tilix fastfetch  unrar unzip cargo p7zip ntfs-3g  ffmpeg ttf-mscorefonts-installer fonts-firacode fonts-jetbrains-mono fonts-croscore fonts-crosextra-carlito fonts-crosextra-caladea fonts-noto fonts-noto-cjk extrepo -yy
+sudo apt install vlc micro libreoffice tilix fastfetch unzip cargo p7zip ntfs-3g  ffmpeg fonts-firacode fonts-jetbrains-mono fonts-croscore fonts-crosextra-carlito fonts-crosextra-caladea fonts-noto fonts-noto-cjk extrepo -yy
 sudo apt purge nano gnome-terminal firefox-esr -yy
 sudo apt autoremove
 # librewolf
